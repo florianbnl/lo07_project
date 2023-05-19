9d2b6900a1fafb91463319002243f5e1acac4db0
fbone <fbone@LAPTOP-7TBE5GMA.lan>
2023-05-20 00:21:35 +0200

    sauvegarde 19/05/23

--- projet/app/controller/ControleurSpecialites.php
+++ projet/app/controller/ControleurSpecialites.php
@@ -2,7 +2,49 @@
 <?php
 
 class ControleurSpecialites {
-    //put your code here
+    public static function SpecialitesReadAll() {
+        $results = ModelSpecialites::getAll();
+        include 'config.php';
+        $vue = $root . '/app/view/specialites/viewAll';
+        if (DEBUG){
+            echo("ControleurSpecialites : SpecialitesReadAll : vue = $vue");
+        }
+        require ($vue);
+        
+    }
+    
+    public static function SpecialitesReadOne(){
+        $specialite_id = $_GET['id'];
+        $results = ModelSpecialites::getOne($specialite_id);
+        
+        include 'config.php';
+        $vue = $root . 'app/view/specialites/viewAll';
+        if (DEBUG){
+            echo("ControleurSpecialites : SpecialitesReadOne : vue = $vue");
+        }
+        require ($vue);
+    }
+    
+    public static function SpecialitesCreate(){
+        require 'config.php';
+        $vue = $root . '/app/view/specialites/viewInsert.php';
+        if (DEBUG){
+            echo("ControleurSpecialites : SpecialitesCreate : vue = $vue");
+        }
+        require ($vue);
+    }
+    
+    public static function SpecialitesCreated(){
+        $results = ModelSpecialites::insert(htmlspecialchars($_GET['nom']));
+        
+        include 'config.php';
+        $vue = $root . '/app/view/specialites/viewInserted.php';
+        if (DEBUG){
+            echo("ControleurSpecialites : SpecialitesCreated : vue = $vue");
+        }
+        require ($vue);
+    }
+    
 }
 ?>
 <!-- fin ControleurSpecialites -->
