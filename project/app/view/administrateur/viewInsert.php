eb84e41b25ee93237ad4986cba27bf559d1d66d0
Clement_gnt <clement.guinet@utt.fr>
2023-06-08 14:38:00 +0200

    Correction et ajouts de fonctionnalités v19

--- project/app/view/administrateur/viewInsert.php
+++ project/app/view/administrateur/viewInsert.php
@@ -14,7 +14,7 @@ require ($root . '/app/view/fragment/fragmentDoctolibHeader.html');
     <h4 class="text-danger">Création d'une nouvelle spécialité</h4>
     <form role="form" method='get' action='router1.php'>
       <div class="form-group">
-        <input type="hidden" name='action' value='specialiteCreated'>        
+        <input type="hidden" name='action' value='specialitesCreated'>        
         <label class='w-25' for="label">Label : </label><input type="text" name='label' value=''> <br/>                                 
       </div>
       <p/>
